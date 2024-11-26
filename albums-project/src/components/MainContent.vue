0d9c0e98dbc4c6a7d0be8b905438bb24b7e7aa31
SalvatoreGiacomazzo <giacomazzo161@gmail.com>
2024-11-26 21:01:29 +0100

    installed axios, sass and created a first vue component

---
+++ albums-project/src/components/MainContent.vue
@@ -0,0 +1,18 @@
+
+<script>
+export default {
+  name: "MainContent",
+};
+</script>
+
+<template>
+  <div>
+    <h1>Hello MainContent!</h1>
+  </div>
+</template>
+
+<style lang="scss">
+h1 {
+  color: red;
+}
+</style>
